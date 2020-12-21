00caf085d5052fba7e8b205521e775b713c03e07
rex706 <apsinakis@me.com>
2020-12-21 16:12:25 -0500

    Initial commit

---
+++ Crc32 Checker/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+
+namespace Crc32_Checker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void FileDropGrid_Drop(object sender, DragEventArgs e)
+        {
+            Crc32OutputTextBox.Text = "";
+
+            var crc32 = new Crc32();
+
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                string file = files[0];
+
+                var hash = String.Empty;
+                using (var fs = File.Open(file, FileMode.Open))
+                    foreach (byte b in crc32.ComputeHash(fs)) hash += b.ToString("x2").ToLower();
+
+                Crc32OutputTextBox.Text = hash;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Crc32 Checker
  Easily drag and drop any file to generate its hash.
+
+[!alt](https://i.imgur.com/UaRRHCs.png)
